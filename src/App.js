d0b0655a1f59cf2a92e55b4621e7e733b52b06df
unknown <v.dauphin41@gmail.com>
2021-05-16 16:28:33 +0200

    Modification mineure index

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Etudier React
         </a>
       </header>
     </div>
